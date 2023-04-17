522268932239bc4d9e25adeb51f663162f61655b
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-04-17 23:37:15 +0200

    upgrade

--- src/useCases/hello/index.ts
+++ src/useCases/hello/index.ts
@@ -1,16 +1,11 @@
 import { z } from 'zod';
-import { tineInput, tineVar } from 'tinejs';
+import { tineVar } from 'tinejs';
 import payload from 'tinejs.payload';
 
-const schema = z.object({ name: z.string().nullable().optional() });
+const input = z.object({ name: z.string().nullable().optional() });
 
-const input = tineInput(schema, {
-  name: 'helloInput',
+const hello = payload({
+  message: tineVar(input, ({ name }) => `Hello ${name || 'World'}`)
 });
-
-const hello = payload(
-  tineVar(input, ({ name }) => `Hello ${name || 'World'}`),
-  { name: 'hello' },
-);
 
 export default hello.withInput(input);
